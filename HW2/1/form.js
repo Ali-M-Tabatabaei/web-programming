a0cd7a10b84e8fa7af5318d50b7391fb5367e925
Ali Mohammad Tabatabaei <85901084+Ali-M-Tabatabaei@users.noreply.github.com>
2023-12-10 20:15:46 +0330

    initiated-HW2-form-submition

---
+++ HW2/1/form.js
@@ -0,0 +1,27 @@
+$('#next1').click(function(){
+    // Validation code here
+    $('#step1').hide();
+    $('#step2').show();
+    $('#progress').css('width', '33%');
+});
+
+$('#next2').click(function(){
+    // Validation code here
+    $('#step2').hide();
+    $('#step3').show();
+    $('#progress').css('width', '66%');
+});
+
+$('#prev1').click(function(){
+    $('#step2').hide();
+    $('#step1').show();
+    $('#progress').css('width', '0');
+});
+
+$('#prev2').click(function(){
+    $('#step3').hide();
+    $('#step2').show();
+    $('#progress').css('width', '33%');
+});
+
+$('#submit').click(function
