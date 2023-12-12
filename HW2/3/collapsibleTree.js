0917ef7932b3495120c4ae3f5993577f8e3f9066
Ali Mohammad Tabatabaei <85901084+Ali-M-Tabatabaei@users.noreply.github.com>
2023-12-12 21:25:46 +0330

    added depth three to the collapsible tree project

--- HW2/3/collapsibleTree.js
+++ HW2/3/collapsibleTree.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
         // Toggle the selected first-level item
         selectedChild.slideToggle();
     });
+    $('.sub-parent').click(function(){
+        var selectedChild = $(this).next('.sub-child');
+        
+        // Close all first-level items except for the selected one
+        $('.sub-child').not(selectedChild).slideUp();
+        
+        // Toggle the selected first-level item
+        selectedChild.slideToggle();
+    });
 });
